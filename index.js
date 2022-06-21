0c39ab3bd2007ab62e6e5862f8956c5fa73a92b1
Agustin <aguspaparo@gmail.com>
2022-06-20 23:23:33 -0300

    primer entrega 20/06

---
+++ index.js
@@ -0,0 +1,17 @@
+// Tabla del numero que elijas, al revez. Usando FOR
+
+
+    let x = prompt ("Tabla del");
+// for (number = 10 ; number >= 0 ; number --){
+//     let tabla = x * number;
+//     console.log ( x + "x" + number + "=" + tabla);
+// }
+
+
+// Tabla del numero que elijas, al revez. Usando DO WHILE
+let i =10
+do{
+    let tabla = x * i;
+    console.log (x + " x" + i + "=" + tabla);
+    i--
+}while ( i>=0)
