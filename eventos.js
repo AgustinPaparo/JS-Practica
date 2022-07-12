371c0c8cf19e9dcdb10eb394d746a9e348a5049d
Agustin <aguspaparo@gmail.com>
2022-07-11 23:46:34 -0300

    Un poco mas de juegos con eventos

--- eventos.js
+++ eventos.js
@@ -10,8 +10,19 @@ function salirDelBoton(){
     boton.innerHTML = "Volve a mi :'("
 }
 
+function pararseEnTitulo() {
+    titulo.innerText = "COMO ELLA CON TU CORA BRO xD"
+}
+
+function salirDeTitulo(){
+    titulo.innerText = "JUGANDO con Eventos."
+}
+
 let boton = document.getElementById ("Boton1");
 boton.addEventListener ("click" , respuestaClick);
 boton.addEventListener ("mouseover" , pararseArriba);
-boton.addEventListener ("mouseout" , salirDelBoton)
+boton.addEventListener ("mouseout" , salirDelBoton);
 
+let titulo = document.getElementById("Titulo");
+titulo.addEventListener ("mouseover" , pararseEnTitulo);
+titulo.addEventListener ("mouseout" , salirDeTitulo);
